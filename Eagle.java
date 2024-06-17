19a1ce6aa908c87759009bb4d660c1382c9a381f
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-06-17 16:10:05 +0200

    Q08 interface

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -19,6 +19,39 @@ public class Eagle extends Bird {
 
     @Override
     public String sing() {
-        return "Screech!";
+        return "glides into the air.";
     }
+    public int flyUp(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude + meters, 200);
+            System.out.printf("%s flies upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    public int flyDown(int meters){
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude - meters, 0);
+            System.out.printf("%s flies downward, altitude : %d%n", this.getName(), this.altitude);
+    }
+ return this.altitude;
+}
+
+    public void land(){
+        if (this.altitude == 1 && this.flying) {
+            this.flying = false;
+            System.out.printf("%s lands.%n", this.getName());
+        }else {
+            System.out.printf("%s can't land.%n", this.getName());}
+    }
+
+    public void fly(){{
+        if (!this.flying) {
+            this.flying = true;
+            System.out.printf("%s takes off.%n", this.getName());
+        }
+    }
+
+
+}
 }
---
+++ Fly.java
@@ -0,0 +1,11 @@
+public interface Fly {
+    
+    void fly();
+
+    int flyDown(int meters);
+
+    int flyUp(int meters);
+
+    void land();
+
+}
